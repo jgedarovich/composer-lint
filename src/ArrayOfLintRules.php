73bd8b4177bcf6805c0871909e7255f2d18081e9
jgedarovich <jgedarovich@etsy.com>
2017-10-19 16:07:18 +0000

    try to get aronud that autoloader doesn't seme to work during composer update (that actually makes sense to me)

--- src/ArrayOfLintRules.php
+++ src/ArrayOfLintRules.php
@@ -30,9 +30,17 @@ class ArrayOfLintRules extends ArrayObject {
 
         if ( in_array($lint_class_name, $this->all_lint_rules) ) {
             $namespaced_lint_class_name="\\".__NAMESPACE__."\\".$lint_class_name;
-            $this->append(new $namespaced_lint_class_name($lint_class_config));
+
+            try {
+                if ( !class_exists($namespaced_lint_class_name) ) {
+                    require dirname(__FILE__).'/LintRules/'.$lint_class_name.".php";
+                }
+                $this->append(new $namespaced_lint_class_name($lint_class_config));
+            } catch(Exception $e) {
+                throw new \InvalidArgumentException("Unable to instantiate lint rule class '$lint_class_name', known lint class names are: ".implode(",", $this->all_lint_rules). " error message: " .$e->getMessage());
+            }
         } else {
-            throw new \InvalidArgumentException('can only add classes of type LintRule');
+            throw new \InvalidArgumentException("Unknown lint rule class '$lint_class_name', known lint class names: ".implode(",", $this->all_lint_rules));
         }
     }
 }
